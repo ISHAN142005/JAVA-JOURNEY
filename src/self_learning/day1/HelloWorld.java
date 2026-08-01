740ee21d5e0491e08afdba78f94f5bef39b90aad
ISHAN142005 <ishanbohra1414@gmail.com>
2026-08-01 22:47:32 +0530

    Hello World

---
+++ src/self_learning/day1/HelloWorld.java
@@ -0,0 +1,8 @@
+package self_learning.day1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my very first Java program!");
+    }
+}
+
