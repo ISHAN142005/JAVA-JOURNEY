ff98474867b4a664ea882aab1cafe2e027c087ac
ISHAN142005 <ishanbohra1414@gmail.com>
2026-08-01 23:08:10 +0530

    Basic Calculator

---
+++ src/self_learning/day1/MyCalculator.java
@@ -0,0 +1,13 @@
+package self_learning.day1;
+
+public class MyCalculator {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 3;
+
+        int sum = number1 + number2;
+
+        System.out.println("I just added " + number1 + " and " + number2 + ".");
+        System.out.println("The answer is: " + sum);
+    }
+}
