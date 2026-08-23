1aecc65dbf1f140f8cc46eabd25de031384f8608
ISHAN142005 <ishanbohra1414@gmail.com>
2026-08-23 23:00:26 +0530

    Add LocalVariableExample demonstrating local variable scope
    - Declared local variable 'var' inside main method
    - Illustrates that local variables are accessible only within their defining method

---
+++ src/college_work/classwork/a/LocalVariableExample.java
@@ -0,0 +1,8 @@
+package college_work.classwork.a;
+
+class LocalVariableExample {
+    public static void main(String[] args) {
+        int var = 10;
+        System.out.println("Local Variable: " + var);
+    }
+}
