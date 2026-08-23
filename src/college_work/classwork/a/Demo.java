4ccfbbf749ead3bd5c9b6e4c2409439e587629f1
ISHAN142005 <ishanbohra1414@gmail.com>
2026-08-23 22:58:41 +0530

    Add Demo class demonstrating static variable usage
    - Declared static variable 'name' in Demo class
    - Accessed static variable directly via class name without object creation
    - Demonstrates static memory allocation at class level

---
+++ src/college_work/classwork/a/Demo.java
@@ -0,0 +1,9 @@
+package college_work.classwork.a;
+
+class Demo {
+    public static String name = "OTS Training";
+
+    public static void main(String[] args) {
+        System.out.println("Static variable is: " + Demo.name);
+    }
+}
