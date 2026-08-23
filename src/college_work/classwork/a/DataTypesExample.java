47d28611f0b66fcf4286d32d46f2b8531dd9ea1b
ISHAN142005 <ishanbohra1414@gmail.com>
2026-08-23 23:02:16 +0530

    Add DataTypesExample demonstrating Java primitive data types
    - Declared and initialized boolean, byte, int, double, char, and String variables
    - Printed each variable to show usage of primitive and reference data types

---
+++ src/college_work/classwork/a/DataTypesExample.java
@@ -0,0 +1,19 @@
+package college_work.classwork.a;
+
+public class DataTypesExample {
+    public static void main(String[] args) {
+        boolean flag = true;
+        byte age = 25;
+        int count = 100;
+        double pi = 3.14159;
+        char grade = 'A';
+        String name = "Jack Ryan";
+
+        System.out.println("Flag: " + flag);
+        System.out.println("Age: " + age);
+        System.out.println("Count: " + count);
+        System.out.println("Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Name: " + name);
+    }
+}
