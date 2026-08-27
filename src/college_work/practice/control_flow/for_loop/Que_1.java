9f8400e7115818b78187dcd6f3b23be133c73af0
ISHAN142005 <ishanbohra1414@gmail.com>
2026-08-27 20:49:44 +0530

    Add program to print numbers from 1 to 10 using for loop

---
+++ src/college_work/practice/control_flow/for_loop/Que_1.java
@@ -0,0 +1,11 @@
+package college_work.practice.control_flow.for_loop;
+
+//Print numbers from 1 to 10
+public class Que_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
