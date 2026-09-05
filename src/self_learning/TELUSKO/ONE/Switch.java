01152828a18b9dfe1be5330d191ec2d148a18fad
ISHAN142005 <ishanbohra1414@gmail.com>
2026-09-05 19:27:03 +0530

    SWITCH CASE

---
+++ src/self_learning/TELUSKO/ONE/Switch.java
@@ -0,0 +1,29 @@
+package self_learning.TELUSKO.ONE;
+
+public class Switch {
+    public static void main(String[]args){
+        int n=1;
+        switch(n){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            default:
+                System.out.println("Wrong");
+        }
+    }
+}
