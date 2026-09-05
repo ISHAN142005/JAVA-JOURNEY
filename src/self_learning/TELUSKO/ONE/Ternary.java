ead438d4e8a6ef097ecb0234acf03ad062bb2355
ISHAN142005 <ishanbohra1414@gmail.com>
2026-09-05 19:17:33 +0530

    TERNARY OPERATOR

---
+++ src/self_learning/TELUSKO/ONE/Ternary.java
@@ -0,0 +1,9 @@
+package self_learning.TELUSKO.ONE;
+
+public class Ternary {
+    public static void main(String[]args){
+        int n=21;
+        int result=n%2==0 ?1:0;
+        System.out.println(result);
+    }
+}
