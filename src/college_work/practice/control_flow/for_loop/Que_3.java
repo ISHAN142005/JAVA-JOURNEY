705b937b6755e94177ed956fb5eae0685dd27b54
ISHAN142005 <ishanbohra1414@gmail.com>
2026-09-06 11:34:13 +0530

    WHILE LOOP

---
+++ src/college_work/practice/control_flow/for_loop/Que_3.java
@@ -0,0 +1,4 @@
+package college_work.practice.control_flow.for_loop;
+
+public class Que_3 {
+}
---
+++ src/self_learning/TELUSKO/ONE/WhileLoop.java
@@ -0,0 +1,17 @@
+package self_learning.TELUSKO.ONE;
+
+public class WhileLoop {
+    public static void main(String[]main){
+        //Nested while loop
+        int i =1;
+        while(i<=7){
+            System.out.println("Day:"+i);
+            int j=1;
+            while(j<=24){
+                System.out.println("Hour:"+j);
+            j++;
+            }
+        i++;
+        }
+    }
+}
