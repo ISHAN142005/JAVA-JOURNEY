ff8a639250007ae5f22746c87d0d912021995587
ISHAN142005 <ishanbohra1414@gmail.com>
2026-09-06 11:44:01 +0530

    DO WHILE LOOP

---
+++ src/self_learning/TELUSKO/ONE/DoWhile.java
@@ -0,0 +1,11 @@
+package self_learning.TELUSKO.ONE;
+
+public class DoWhile {
+    public static void main(String[]args){
+        int i=1;
+        do{
+            System.out.println("Hello Once.");
+            i++;
+        }while(i<=2);
+    }
+}
--- src/self_learning/TELUSKO/ONE/SwitchNewVersion.java
+++ src/self_learning/TELUSKO/ONE/SwitchNewVersion.java
@@ -4,7 +4,7 @@ public class SwitchNewVersion {
     public static void main(String[] args) {
         int n = 2;
         String day = switch (n) {
-            case 1 -> "Monday";
+            case 1 -> "Monday";//case 1:yield "Monday"
             case 2 -> "Tuesday";
             case 3 -> "Wednesday";
             default -> "Invalid day";
