f12a0faf2b53c6415f39fa75dc03d7016f353c47
ISHAN142005 <ishanbohra1414@gmail.com>
2026-09-14 10:20:00 +0530

    STRING UNIQUE COUNT

---
+++ src/college_work/classwork/UniqueCount.java
@@ -0,0 +1,26 @@
+package college_work.classwork;
+
+public class UniqueCount {
+    public static void main(String[] args) {
+
+        String str = "RANJANA";
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            boolean unique = true;
+
+            for (int j = 0; j < i; j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    unique = false;
+                    break;
+                }
+            }
+
+            if (unique) {
+                count++;
+            }
+        }
+
+        System.out.println("Unique characters: " + count);
+    }
+}
